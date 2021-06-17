bf0c90a4f020796916756ff581ce19d38d3ab819
GnudnaoD <khongphaidoandung@gmail.com>
2021-06-17 23:13:14 +0700

    Chat server client

---
+++ SecureChat/Client/Client.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace Client
+{
+    public partial class Client : Form
+    {
+        public Client()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+
+        IPEndPoint IP;
+        Socket client;
+
+        void Connect()
+        {
+            IP = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 9999);
+            client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            try
+            {
+                client.Connect(IP);
+            }
+            catch
+            {
+                MessageBox.Show("Cannot connect", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            
+
+            Thread listen = new Thread(Receive);
+            listen.IsBackground = true;
+            listen.Start();
+        }
+
+        void Close()
+        {
+            client.Close();
+        }
+
+        void Send()
+        {
+            if (textBox1.Text != string.Empty)
+                client.Send(Serialize(YourName.Text + ": "+ textBox1.Text));
+            textBox1.Text = "";
+        }
+
+        void Receive()
+        {
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 1000];
+                    client.Receive(data);
+
+                    string message = (string)Deserialize(data);
+                    richTextBox1.Text +=message + "\r\n";
+                }
+            }
+            catch
+            {
+                Close();
+            }
+        }
+
+        byte[] Serialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, obj);
+
+            return stream.ToArray();
+        }
+
+        object Deserialize(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return formatter.Deserialize(stream);
+        }
+
+        private void Client_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Send();
+        }
+    }
+}
---
+++ SecureChat/WindowsFormsApp1/Server.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Threading;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace WindowsFormsApp1
+{
+    public partial class Server : Form
+    {
+        public Server()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false;
+            Connect();
+        }
+        IPEndPoint IP;
+        Socket server;
+        List<Socket> clientList;
+
+        void Connect()
+        {
+            clientList = new List<Socket>();
+
+            IP = new IPEndPoint(IPAddress.Any, 9999);
+            server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+
+            server.Bind(IP);
+
+            Thread Listen = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        server.Listen(100);
+                        Socket client = server.Accept();
+
+                        clientList.Add(client);
+
+                        Thread receive = new Thread(Receive);
+                        receive.IsBackground = true;
+                        receive.Start(client);
+                    }
+                }
+                catch
+                {
+                    IP = new IPEndPoint(IPAddress.Any, 9999);
+                    server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.IP);
+                }
+
+
+            });
+            Listen.IsBackground = true;
+            Listen.Start();
+        }
+
+        void Receive(object obj)
+        {
+            Socket client = obj as Socket;
+            try
+            {
+                while (true)
+                {
+                    byte[] data = new byte[1024 * 1000];
+                    client.Receive(data);
+
+                    string message = (string)Deserialize(data);
+                    //listView1.Items.Add(new ListViewItem() { Text = message });
+                
+                    foreach (Socket item in clientList)
+                    {
+                        if (item != null)
+                        {
+                            item.Send(Serialize(message));
+                        }
+                    }
+                }
+            }
+            catch
+            {
+                clientList.Remove(client);
+                client.Close();
+            }
+        }
+
+        byte[] Serialize(object obj)
+        {
+            MemoryStream stream = new MemoryStream();
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            formatter.Serialize(stream, obj);
+
+            return stream.ToArray();
+        }
+
+        object Deserialize(byte[] data)
+        {
+            MemoryStream stream = new MemoryStream(data);
+            BinaryFormatter formatter = new BinaryFormatter();
+
+            return formatter.Deserialize(stream);
+        }
+    }
+}
